777fcddb69b5301069f03d25cc492c8f52173319
ncale <nicholasbrodeur@outlook.com>
2024-03-12 15:13:24 -0500

    split out fetch functions

--- src/app/api/delegates/route.ts
+++ src/app/api/delegates/route.ts
@@ -1,24 +1,45 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { Dune } from 'dune-api-client'
 import { DUNE_API_KEY, DUNE_DELEGATE_QUERY_ID } from "@/utils/config";
 
-export async function GET(request: NextRequest) {
-	const res = await getDuneData()
-	return res
+export async function GET() {
+	const delegates = await getDelegates();
+	return NextResponse.json(delegates)
 }
 
-export async function getDuneData() {
-	const dune = new Dune(DUNE_API_KEY)
-
-	const execute = await dune.execute(DUNE_DELEGATE_QUERY_ID)
-	const res = await dune.results<DuneDelegateData>(execute.execution_id)
+export async function getDelegates() {
+	const duneData = await fetchDuneData()
+	if (!duneData) return
+	const formattedDelegateData = duneData.map((delegate) => {
+		return {
+			rank: delegate.delegate_rank,
+			address: delegate.delegate,
+			username: delegate.delegate_name_raw,
+			voting_power: delegate.dt_voting_power,
+			pct_voting_power: delegate.pct_voting_power
+		} as FormattedDelegate
+	})
+	return formattedDelegateData
 }
 
-interface DuneDelegateData {
-	primary_ens_name?: string
+interface FormattedDelegate {
+	rank: number
 	address: string
-	pfp?: string
+	username: string
 	voting_power: number
-	percent_delegated_supply: number
-	percent_participation: number
+	pct_voting_power: number
+}
+
+export async function fetchDuneData() {
+	const dune = new Dune(DUNE_API_KEY)
+	const res = await dune.results<DuneDelegateQuery>(DUNE_DELEGATE_QUERY_ID)
+	if (res.result) return res.result.rows
+}
+
+interface DuneDelegateQuery {
+	delegate_rank: number
+	delegate: string
+	delegate_name_raw: string
+	dt_voting_power: number
+	pct_voting_power: number
 }
