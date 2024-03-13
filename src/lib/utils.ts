83818280e40e903e30ef620abd5cd44e3b1015b1
ncale <nicholasbrodeur@outlook.com>
2024-03-13 14:22:40 -0500

    add govscore calculation function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,28 @@ export function formatPercentValue(num: number) {
 	if (formattedNum === '0.00%') return '<0.01%'
 	return formattedNum
 }
+
+export function calcGovScore(
+	isEnsNameSet: boolean, 
+	isEnsAvatarSet: boolean, 
+	isFcAcctAttached: boolean,
+	recentParticipationRatio: number,
+	pctDelegation: number,
+) {
+	let govScore = 0
+	// add transparency criteria
+	if (isEnsNameSet) govScore++
+	if (isEnsAvatarSet) govScore++
+	if (isFcAcctAttached) govScore++
+	// add consistency criteria
+	govScore += (recentParticipationRatio * 0.5)
+	// add power balance criteria
+	if (pctDelegation < 0.005) {
+		govScore += 3
+	} else if (pctDelegation < 0.01) {
+		govScore += 2
+	} else if (pctDelegation < 0.015) {
+		govScore += 1
+	}
+	return govScore
+}
