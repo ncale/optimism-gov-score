7304d58a8c8d6387e0510a78fd1e073522281c3c
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-03-12 20:29:47 -0500

    Add rainbow kit (#5)
    * add rainbow kit
    
    * remove old connect button component
    
    * add wallet connect project id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
 
 import { Providers } from "@/components/Providers";
 import Header from "@/components/Header";
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,33 @@
 'use client'
 
-import {NextUIProvider} from '@nextui-org/react'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WC_PROJECT_ID } from '@/config/config';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { WagmiProvider } from 'wagmi';
+import { optimism } from 'wagmi/chains';
+import { NextUIProvider } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
+
+const config = getDefaultConfig({
+  appName: 'GovScore',
+  projectId: WC_PROJECT_ID,
+  chains: [optimism],
+  ssr: true,
+});
+const queryClient = new QueryClient();
 
 export function Providers({children}: { children: React.ReactNode }) {
   const router = useRouter();
 	
 	return (
-    <NextUIProvider navigate={router.push}>
-      {children}
-    </NextUIProvider>
+    <WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<RainbowKitProvider>
+					<NextUIProvider navigate={router.push}>
+						{children}
+					</NextUIProvider>
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
   )
 }
