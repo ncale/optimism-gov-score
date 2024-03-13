00348c632af1bdce4372660b2bc87cfd91ca1d9b
ncale <nicholasbrodeur@outlook.com>
2024-03-13 14:23:06 -0500

    add op gov token contract address and abi

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,4 +4,5 @@ export const WC_PROJECT_ID = process.env.WC_PROJECT_ID || 'PROJECT_ID'
 export const DUNE_API_KEY = process.env.DUNE_API_KEY || ''
 
 // configure global variables
+export const OP_CONTRACT_ADDRESS = '0x4200000000000000000000000000000000000042'
 export const DUNE_DELEGATE_QUERY_ID = 3517653
