bc57bf4a9fcf5d454566df6d911a4fef625483b0
ncale <nicholasbrodeur@outlook.com>
2024-03-19 15:42:33 +0000

    temporarily remove api to avoid build errors

--- src/app/api/delegates/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getDelegates } from "@/services/getDelegates";
-
-export async function GET() {
-	const delegates = await getDelegates();
-	return NextResponse.json(delegates)
-}
--- src/app/api/votes/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { fetchVotes } from "@/services/fetchVotes";
-
-export async function GET() {
-	const delegates = await fetchVotes();
-	return NextResponse.json(delegates)
-}
