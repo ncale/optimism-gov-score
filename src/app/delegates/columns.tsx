9bf6a6ec8696b8f5d6c681ebd128fb87342799a8
ncale <nicholasbrodeur@outlook.com>
2024-03-19 21:50:10 +0000

    update gov score cell

--- src/app/delegates/columns.tsx
+++ src/app/delegates/columns.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+// Components
 import { ColumnDef, createColumnHelper } from "@tanstack/react-table"
-import { formatBigNumber, formatPercentValue } from "@/lib/utils";
-import DelegateButton from "./delegate-button";
-import GovScoreCell from "./gov-score-cell";
-import { useEnsName, useEnsAvatar } from "wagmi";
-import { normalize } from "viem/ens";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import type { CellContext } from "@tanstack/react-table";
+import { Tooltip } from "@nextui-org/react";
+import DelegateButton from "./delegate-button";
+// Hooks
+import { useEnsName, useEnsAvatar } from "wagmi";
+// Helper functions
+import { formatBigNumber, formatPercentValue, calcGovScore } from "@/lib/utils";
+import { normalize } from "viem/ens";
+// Types
+import type { CellContext, Row } from "@tanstack/react-table";
+// Icons
+import { IconContext } from "react-icons/lib";
+import { LuCheckCircle2 } from "react-icons/lu";
+import { LuMinusCircle } from "react-icons/lu";
+import { LuXCircle } from "react-icons/lu";
 
 const columnHelper = createColumnHelper<DelegateTableRow>()
 
@@ -43,7 +52,7 @@ export const columns = [
 		header: () => <div className="head">Recent Participation</div>,
 		cell: ({ row }) => {
 			const voteCount = row.getValue('count_participation')
-			return <div className="cell">{`${voteCount}/10`}</div>
+			return <div className="cell">{`${voteCount}/10 votes`}</div>
 		}
 	}),
 	columnHelper.accessor('is_current_delegate', {
@@ -93,3 +102,103 @@ function DelegateCell({ props }: { props: CellContext<DelegateTableRow, string> 
 		</a>
 	)
 }
+
+function GovScoreCell({ row }: { row: Row<DelegateTableRow> }) {
+	const { data: ensName } = useEnsName({
+		address: row.original.address,
+		chainId: 1
+	})
+	const { data: ensAvatar } = useEnsAvatar({
+		name: normalize(ensName?.toString() ?? ''),
+		chainId: 1
+	})
+	const voteCount = row.original.count_participation
+	const govScoreConfig = {
+		isEnsNameSet: (typeof ensName === 'string' && ensName.length > 0),
+		isEnsAvatarSet: (typeof ensAvatar === 'string' && ensAvatar.length > 0),
+		isFcAcctAttached: false, // dummy data
+		recentParticipation: voteCount,
+		pctDelegation: row.original.pct_voting_power,
+	}
+	const { scores } = calcGovScore(govScoreConfig)
+	const govScore = Object.values(scores).reduce((a, b) => a + b, 0)
+	function getPctDelegationText(score: number) {
+		switch (score) {
+			case 0:
+				return "More than 1.5%"
+			case 1: 
+				return "Less than 1.5%"
+			case 2:
+				return "Less than 1.0%"
+			case 3:
+				return "Less than 0.5%"
+		}
+	}
+	const pctDelegationText = getPctDelegationText(scores.pctDelegation)
+	return (
+		<div className="cell col-gov-score">
+			<Tooltip 
+				placement="right"
+				content={
+					<div>
+						<div className="tooltip-text">
+							{scores.ensName === 1 ? <CheckIcon /> : <XMarkIcon />}
+							<ScorePill score={scores.ensName} />
+							<span className="line">{govScoreConfig.isEnsNameSet ? "" : "No "} ENS Primary Name Set</span>
+						</div>
+						<div className="tooltip-text">
+							{scores.ensAvatar === 1 ? <CheckIcon /> : <XMarkIcon />}
+							<ScorePill score={scores.ensAvatar} />
+							<span className="line">{govScoreConfig.isEnsAvatarSet ? "" : "No "} ENS Avatar Set</span>
+						</div>
+						{/* <div className="tooltip-text">
+							<ScoreCircle num={scores.fcAcct} />
+							[WIP] {govScoreConfig.isFcAcctAttached ? "" : "No "} Detected Farcaster Account
+						</div> */}
+						<div className="tooltip-text">
+							{scores.recentParticipation > 3.5 ? <CheckIcon /> : (scores.recentParticipation > 1.5 ? <EmptyIcon /> : <XMarkIcon />)}
+							<ScorePill score={scores.recentParticipation} />
+							<span className="line">Voted in <span className="special">{voteCount}</span> of last <span className="special">10</span> onchain proposals</span>
+						</div>
+						<div className="tooltip-text">
+							{scores.pctDelegation === 3 ? <CheckIcon /> : (scores.pctDelegation > 0 ? <EmptyIcon /> : <XMarkIcon />)}
+							<ScorePill score={scores.pctDelegation} />
+							<span className="line">{pctDelegationText} of total delegated OP</span>
+						</div>
+					</div>
+				}
+			>
+				<span>{`${govScore}/10`}</span>
+			</Tooltip>
+		</div>
+	)
+}
+
+function CheckIcon() {
+	return (
+		<IconContext.Provider value={{ color: "green", size: "1.15em" }}>
+			<LuCheckCircle2 />
+		</IconContext.Provider>
+	)
+}
+function EmptyIcon() {
+	return (
+		<IconContext.Provider value={{ color: "orange", size: "1.15em" }}>
+			<LuMinusCircle />
+		</IconContext.Provider>
+	)
+}
+function XMarkIcon() {
+	return (
+		<IconContext.Provider value={{ color: "red", size: "1.15em" }}>
+			<LuXCircle />
+		</IconContext.Provider>
+	)
+}
+function ScorePill({ score }: { score: number}) {
+	return (
+		<div className={`rounded-full text-xs text-center w-6 h-4 bg-gray-600 text-white font-bold`}>
+			{score}
+		</div>
+	);
+};
