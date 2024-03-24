ad79153728f897b674c172ee62e6745d916b7775
ncale <nicholasbrodeur@outlook.com>
2024-03-24 18:55:37 -0500

    update thesis page

--- src/app/thesis/page.tsx
+++ src/app/thesis/page.tsx
@@ -1,15 +1,29 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import Image from "next/image";
 
 export default function Home() {
   return (
 		<main className="main thesis-page">
-			<h2>Thesis</h2>
-			<p>
-				In the world of DAO governance and leadership, three key pillars stand out as 
-				essential components for ensuring long-lasting and successful outcomes: <span>transparency</span>, 
-				<span> consistency</span>, and <span>balance of power</span>.
-			</p>
-			<Image src='/infographic.png' alt='GovScore infographic' width={1995} height={1258} className="infographic"/>
+			<Accordion type="single" collapsible className='mx-auto w-11/12 md:w-readWidth'>
+				<AccordionItem value="item-1">
+					<AccordionTrigger>Why GovScore?</AccordionTrigger>
+					<AccordionContent>
+					In the world of DAO governance and leadership, we see two core concerns among those in the space: first, the slow 
+					but consistent consolidation of power, and second, the concern of waning engagement among delegates. We believe strongly 
+					in the potential of decentralized governance, but to unlock that potential, we need to start with systems that hold 
+					delegates accountable. Enter GovScore: what we see as the first step in ensuring that those who decide the future of
+					DAOs are consistently the best for the job.
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+			<Accordion type="single" collapsible className="mx-auto w-11/12 md:w-readWidth">
+				<AccordionItem value="item-1">
+					<AccordionTrigger>How did you pick the criteria?</AccordionTrigger>
+					<AccordionContent>
+					wip
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
 		</main>
 	);
 };
