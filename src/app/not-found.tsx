a70d344000dffa809cb7b6d4e27b40bd2a85296c
ncale <nicholasbrodeur@outlook.com>
2024-03-24 20:21:19 -0500

    escape apos char

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
 		<main className="main text-center">
-			<p className="mt-8 mb-2">Uh oh... we couldn't find the page you were looking for.</p>
+			<p className="mt-8 mb-2">Uh oh... we couldn&apos;t find the page you were looking for.</p>
 			<p>Return to the <Link href={"/faq"} className="special link">FAQ</Link> or <Link href={"/delegates"} className="special link">Delegates</Link> page.</p>
 		</main>
 	);
