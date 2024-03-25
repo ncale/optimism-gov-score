eaf0683c666a21620ccc471b89b246c08ced5650
ncale <nicholasbrodeur@outlook.com>
2024-03-24 19:56:28 -0500

    add menu icon to mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,14 +5,14 @@ export default function Header() {
 	return (
 		<Navbar isBordered height="6rem" maxWidth="full">
 			<NavbarBrand className="flex flex-col items-start">
-        <Image 
+				<Image 
 					src={"/optimism-logo.png"} 
 					alt="optimism logo"
 					width={225}
 					height={31}
-					className="h-6 w-auto"
+					className="h-5 md:h-6 w-auto"
 					radius="none" />
-				<h1 className="text-2xl text-inherit">GovScore</h1>
+				<h1 className="text-xl md:text-2xl text-inherit">GovScore</h1>
       </NavbarBrand>
 
       <NavLinks />
--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -3,32 +3,45 @@
 import { NavbarContent, NavbarItem, Link } from "@nextui-org/react";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { usePathname } from "next/navigation";
+import HamburgerIcon from "./icons/hamburger-icon";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export function NavLinks() {
+	const isDesktop = useMediaQuery("(min-width: 768px)")
 	const currentPath = usePathname();
 	return (
-		<NavbarContent className="hidden sm:flex gap-4 tracking-wide" justify="center">
-			<NavbarItem className="text-xl">
-				<Link href="/faq" color="foreground" className={currentPath === "/faq" ? "font-bold" : ""}>
-					FAQ
-				</Link>
-			</NavbarItem>
-			<NavbarItem className="text-xl">|</NavbarItem>
-			<NavbarItem className="text-xl">
-				<Link href="/delegates" color="foreground" className={currentPath === "/delegates" ? "font-bold" : ""}>
-					Delegates
-				</Link>
-			</NavbarItem>
-		</NavbarContent> 
+		<>
+			{isDesktop ? (
+				<NavbarContent className="flex gap-4 tracking-wide" justify="center">
+					<NavbarItem className="text-xl">
+						<Link href="/faq" color="foreground" className={currentPath === "/faq" ? "font-bold" : ""}>
+							FAQ
+						</Link>
+					</NavbarItem>
+					<NavbarItem className="text-xl">|</NavbarItem>
+					<NavbarItem className="text-xl">
+						<Link href="/delegates" color="foreground" className={currentPath === "/delegates" ? "font-bold" : ""}>
+							Delegates
+						</Link>
+					</NavbarItem>
+				</NavbarContent>
+			) : null}
+		</>
 	)
 }
 
 export function NavConnectButton() {
+	const isDesktop = useMediaQuery("(min-width: 768px)")
 	return (
 		<NavbarContent justify="end">
 			<NavbarItem>
 				<ConnectButton chainStatus="none" showBalance={false} accountStatus={{ smallScreen: "avatar", largeScreen: "address" }}/>
 			</NavbarItem>
+			{isDesktop ? null : (
+				<NavbarItem>
+					<HamburgerIcon />
+				</NavbarItem>
+			)}
 		</NavbarContent>
 	)
 }
