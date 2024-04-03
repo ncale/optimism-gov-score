758ea57eecca68029e4436fb379bb7d0de022e5b
ncale <nicholasbrodeur@outlook.com>
2024-04-03 16:15:14 -0400

    prettify code and reword button for clarity

--- src/app/delegates/delegate-button.tsx
+++ src/app/delegates/delegate-button.tsx
@@ -1,45 +1,52 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import { opTokenAbi } from "@/config/op-token-abi";
 import { OP_TOKEN_ADDRESS } from "@/config/config";
 
-export default function DelegateButton({ newDelegateeAddr }: { newDelegateeAddr: `0x${string}` }) {
-	
-	const { isConnected, address } = useAccount()
-	const { data: delegateAddress } = useReadContract({
-		address: OP_TOKEN_ADDRESS,
-		abi: opTokenAbi,
-		functionName: "delegates",
-		args: [address ?? "0x"],
-		chainId: 10
-	})
-	const { writeContract } = useWriteContract()
+export default function DelegateButton({
+  newDelegateeAddr,
+}: {
+  newDelegateeAddr: `0x${string}`;
+}) {
+  const { isConnected, address } = useAccount();
+  const { data: delegateAddress } = useReadContract({
+    address: OP_TOKEN_ADDRESS,
+    abi: opTokenAbi,
+    functionName: "delegates",
+    args: [address ?? "0x"],
+    chainId: 10,
+  });
+  const { writeContract } = useWriteContract();
 
-	const isCurrentDelegate = delegateAddress ? (newDelegateeAddr.toLowerCase() === delegateAddress.toLowerCase()) : false
+  const isCurrentDelegate = delegateAddress
+    ? newDelegateeAddr.toLowerCase() === delegateAddress.toLowerCase()
+    : false;
 
-	const burnAddress = new RegExp('0x0000000000')
-	const hasDelegate = delegateAddress ? !(burnAddress.test(delegateAddress)) : false
+  const burnAddress = new RegExp("0x0000000000");
+  const hasDelegate = delegateAddress
+    ? !burnAddress.test(delegateAddress)
+    : false;
 
-	function handleClick() {
-		writeContract({
-			abi: opTokenAbi,
-			address: OP_TOKEN_ADDRESS,
-			functionName: 'delegate',
-			account: address,
-			args: [newDelegateeAddr],
-			chainId: 10
-		})
-	}
+  function handleClick() {
+    writeContract({
+      abi: opTokenAbi,
+      address: OP_TOKEN_ADDRESS,
+      functionName: "delegate",
+      account: address,
+      args: [newDelegateeAddr],
+      chainId: 10,
+    });
+  }
 
-	return (
-		<Button  
-			size="xs" 
-			disabled={!isConnected || isCurrentDelegate || !hasDelegate} 
-			onClick={handleClick}
-		>
-			<div>delegate</div>
-		</Button>
-	)
+  return (
+    <Button
+      size="xs"
+      disabled={!isConnected || isCurrentDelegate || !hasDelegate}
+      onClick={handleClick}
+    >
+      <div>redelegate</div>
+    </Button>
+  );
 }
