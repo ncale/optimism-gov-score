e90dc67faf0ae53365d40fa93d1ee030da1e4db9
ncale <nicholasbrodeur@outlook.com>
2024-04-12 10:27:58 -0400

    add feedback url

--- src/components/feedback-flag.tsx
+++ src/components/feedback-flag.tsx
@@ -4,8 +4,9 @@ import { useMediaQuery } from "@/hooks/use-media-query";
 
 export default function FeedbackFlag() {
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const feedbackUrl = "https://forms.gle/ZLTRfZ7z1PwUTGao9";
   return isDesktop ? (
-    <a href="" target="_blank">
+    <a href={feedbackUrl} target="_blank">
       <div className="fixed top-1/4 -right-[3.8rem] w-36 h-10 p-1 bg-red-700 text-white font-bold -rotate-90 rounded-md text-center hover:-right-[3.6rem] ease-in-out duration-75">
         Give Feedback
       </div>
