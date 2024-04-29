7dd54d8d1eb326cb96c309e06a65741c8e89b764
ncale <nicholasbrodeur@outlook.com>
2024-04-29 16:13:04 -0500

    remove next ui header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,11 @@
-import { Navbar, NavbarBrand, Image } from "@nextui-org/react";
 import { NavLinks, NavConnectButton } from "./nav-items";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <Navbar isBordered height="6rem" maxWidth="full">
-      <NavbarBrand>
+    <nav className="fixed z-50 grid h-24 w-full grid-cols-2 items-center border-b bg-background/[0.95] px-4 md:grid-cols-3">
+      <div>
         <Link href={"/"} className="flex flex-col items-start">
           <Image
             src={"/optimism-logo.png"}
@@ -13,15 +13,14 @@ export default function Header() {
             width={225}
             height={31}
             className="h-5 w-auto md:h-6"
-            radius="none"
           />
           <h1 className="text-xl text-inherit md:text-2xl">GovScore</h1>
         </Link>
-      </NavbarBrand>
+      </div>
 
       <NavLinks />
 
       <NavConnectButton />
-    </Navbar>
+    </nav>
   );
 }
