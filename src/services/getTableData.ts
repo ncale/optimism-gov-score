6faaf9f76800c97621b91915260c87574332d7bd
ncale <nicholasbrodeur@outlook.com>
2024-04-29 12:42:15 -0500

    change testing data for debugging

--- src/services/getTableData.ts
+++ src/services/getTableData.ts
@@ -62,7 +62,7 @@ export async function getTableData() {
       pct_voting_power: pct_voting_power,
       recent_participation: recent_participation,
       recent_participation_with_reason: recent_participation_with_reason,
-      testing__data: delegate.votes.items,
+      testing__data: delegate,
     } as DelegateTableRow;
   });
 
