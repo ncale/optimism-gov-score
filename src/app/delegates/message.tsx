0026f56cf8829dba26ef815c32b97bbc0a7b7ec1
ncale <nicholasbrodeur@outlook.com>
2024-04-29 13:09:55 -0500

    update 'your delegate' style

--- src/app/delegates/message.tsx
+++ src/app/delegates/message.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAccount, useReadContract } from "wagmi";
+import { useAccount, useEnsName, useReadContract } from "wagmi";
 import { opTokenAbi } from "@/config/op-token-abi";
 import { OP_TOKEN_ADDRESS } from "@/config/config";
 import { DelegateTableRow } from "./columns";
@@ -13,6 +13,10 @@ export default function Message({
   delegateData: DelegateTableRow[] | undefined;
 }) {
   const { address } = useAccount();
+  const { data: ensName } = useEnsName({
+    address,
+    chainId: 1,
+  });
   const { data: delegateAddress } = useReadContract({
     address: OP_TOKEN_ADDRESS,
     abi: opTokenAbi,
@@ -34,7 +38,7 @@ export default function Message({
 
   if (!delegateAddress || new RegExp("0x000000000000").test(delegateAddress)) {
     return (
-      <div className="flex flex-col items-center justify-center mx-auto p-4 text-center bg-muted rounded w-11/12 md:w-1/2 shadow-sm">
+      <div className="flex flex-col items-center justify-center mx-auto p-4 text-center bg-muted rounded-md border w-11/12 md:w-1/2">
         You haven&apos;t delegated OP
       </div>
     );
@@ -50,7 +54,7 @@ export default function Message({
 
   if (!delegate)
     return (
-      <div className="flex flex-col items-center justify-center mx-auto p-4 text-center bg-muted rounded w-11/12 md:w-1/2">
+      <div className="flex flex-col items-center rounded-md border justify-center mx-auto p-4 text-center bg-muted w-11/12 md:w-1/2">
         Error - Your delegate isn&apos;t in our list. We apologize for the
         inconvenience and are working on a solution
       </div>
@@ -59,14 +63,26 @@ export default function Message({
   const formattedOpBalance = opBalance ? formatEther(opBalance) : null;
 
   return (
-    <div className="flex flex-col mx-auto py-4 px-4 md:px-8 bg-muted rounded w-11/12 md:w-[36rem] gap-1.5 shadow-md">
-      {formattedOpBalance ? (
-        <OPBalanceCard balance={formattedOpBalance} />
-      ) : (
-        <></>
-      )}
-      {formattedOpBalance && delegate ? <hr /> : <></>}
-      {delegate ? <DelegateCard delegate={delegate} /> : <></>}
+    <div className="mx-auto space-y-2 w-11/12 md:w-[36rem]">
+      <div className="flex space-x-2">
+        {ensName ? (
+          <div className="py-2 px-4 rounded-xl border font-medium">
+            {ensName}
+          </div>
+        ) : (
+          <></>
+        )}
+        {formattedOpBalance ? (
+          <div className="py-2 px-4 rounded-xl border">
+            <OPBalanceCard balance={formattedOpBalance} />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="py-2 px-4 rounded-md border gap-1.5">
+        {delegate ? <DelegateCard delegate={delegate} /> : <></>}
+      </div>
     </div>
   );
 }
