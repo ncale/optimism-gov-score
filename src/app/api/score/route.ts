ef3961b5ab23ce72670f160abfbc75ad3c9db83c
ncale <nicholasbrodeur@outlook.com>
2024-05-09 15:59:05 -0500

    remove api endpoints to avoid build errors

--- src/app/api/score/route.ts
+++ src/app/api/score/route.ts
@@ -1,47 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
 import { formatEther, isAddress } from "viem";
 import { findOneDelegate } from "@/services/getDelegateData";
-import { GovScoreConfig, calcGovScore } from "@/lib/utils";
+import { GovScoreConfig, calculateGovScore } from "@/lib/utils";
 import { QUALIFYING_PROPOSAL_IDS } from "@/config/config";
 
 export const runtime = "edge";
 
-export async function GET(req: NextRequest) {
-  // parse url param and validate format
-  const address = req.nextUrl.searchParams.get("address");
-  if (!address || !isAddress(address)) {
-    return NextResponse.json({ error: "Invalid address" });
-  }
-  return NextResponse.json({
-    status: 500,
-    message: "Server error. This endpoint is not currently maintained.",
-  });
+// export async function GET(req: NextRequest) {
+//   // parse url param and validate format
+//   const address = req.nextUrl.searchParams.get("address");
+//   if (!address || !isAddress(address)) {
+//     return NextResponse.json({ error: "Invalid address" });
+//   }
+//   return NextResponse.json({
+//     status: 500,
+//     message: "Server error. This endpoint is not currently maintained.",
+//   });
 
-  // const res = await findOneDelegate(address);
-  // const delegate = res.data.delegates.items[0];
+//   // const res = await findOneDelegate(address);
+//   // const delegate = res.data.delegates.items[0];
 
-  // // calculate percent of voting power
-  // const currentVotableOP = BigInt("87424148000000000000000000"); // as of 4/11/24
-  // const currentVotableOP_num = Number(formatEther(currentVotableOP));
-  // const votingPower_num = Number(formatEther(BigInt(delegate.votingPower)));
-  // const pct_voting_power = votingPower_num / currentVotableOP_num;
+//   // // calculate percent of voting power
+//   // const currentVotableOP = BigInt("87424148000000000000000000"); // as of 4/11/24
+//   // const currentVotableOP_num = Number(formatEther(currentVotableOP));
+//   // const votingPower_num = Number(formatEther(BigInt(delegate.votingPower)));
+//   // const pct_voting_power = votingPower_num / currentVotableOP_num;
 
-  // // calculate recent voting participation
-  // const proposalsVotedOn = delegate.votes.items.map((vote) => vote.proposalId);
-  // const nonDuplicateVotes = [...Array.from(new Set(proposalsVotedOn))];
-  // const recent_participation = nonDuplicateVotes.filter((proposal) => {
-  //   return QUALIFYING_PROPOSAL_IDS.includes(proposal);
-  // }).length;
+//   // // calculate recent voting participation
+//   // const proposalsVotedOn = delegate.votes.items.map((vote) => vote.proposalId);
+//   // const nonDuplicateVotes = [...Array.from(new Set(proposalsVotedOn))];
+//   // const recent_participation = nonDuplicateVotes.filter((proposal) => {
+//   //   return QUALIFYING_PROPOSAL_IDS.includes(proposal);
+//   // }).length;
 
-  // // combine and calculate govscore
-  // const govScoreConfig: GovScoreConfig = {
-  //   recentParticipation: recent_participation,
-  //   pctDelegation: pct_voting_power,
-  //   isEnsNameSet: !!delegate.ensName,
-  //   isEnsAvatarSet: !!delegate.ensAvatar,
-  //   recentParticipationWithReason: 0, // dummy data
-  // };
-  // const { govScore, scores } = calcGovScore(govScoreConfig);
+//   // // combine and calculate govscore
+//   // const govScoreConfig: GovScoreConfig = {
+//   //   recentParticipation: recent_participation,
+//   //   pctDelegation: pct_voting_power,
+//   //   isEnsNameSet: !!delegate.ensName,
+//   //   isEnsAvatarSet: !!delegate.ensAvatar,
+//   //   recentParticipationWithReason: 0, // dummy data
+//   // };
+//   // const { govScore, scores } = calcGovScore(govScoreConfig);
 
-  // return NextResponse.json({ scores, govScore });
-}
+//   // return NextResponse.json({ scores, govScore });
+// }
