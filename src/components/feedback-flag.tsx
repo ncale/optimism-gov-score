2d560b97916794dd688d140394bb1682daf562a8
ncale <nicholasbrodeur@outlook.com>
2024-05-17 15:38:56 -0500

    fix feedback flag getting covered by other page elements

--- src/components/feedback-flag.tsx
+++ src/components/feedback-flag.tsx
@@ -7,7 +7,7 @@ export default function FeedbackFlag() {
   const feedbackUrl = "https://forms.gle/k6VdUY2JrvxYZkrj7";
   return isDesktop ? (
     <a href={feedbackUrl} target="_blank">
-      <div className="fixed -right-[3.8rem] top-1/4 h-10 w-36 -rotate-90 rounded-md bg-optimism-red p-1 text-center font-bold text-white duration-150 ease-in-out hover:-right-[3.55rem]">
+      <div className="fixed -right-[3.8rem] top-1/4 z-50 h-10 w-36 -rotate-90 rounded-md bg-optimism-red p-1 text-center font-bold text-white duration-150 ease-in-out hover:-right-[3.55rem]">
         Give Feedback
       </div>
     </a>
